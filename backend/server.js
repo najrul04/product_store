f7c48ce39c889c92d8ba3d4d592ae13b5142c8ba
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2024-11-07 21:00:35 +0600

    added-app-gget-*-from--anywhere-loggic

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use('/api/products', productRoutes);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
 }
 
 app.listen(PORT, () => {
